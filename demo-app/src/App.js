9ab06fd215d117f33af2578c909cef601cfb5613
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-09-30 17:42:01 +0530

    Class and Function component

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Car from './components/Car'
+import MyCar from './components/MyCar'
 
 function App() {
   return (
     <div className="App">
       <h1>Hellow World</h1>
+      <Car />
+      <MyCar />
     </div>
   );
 }
---
+++ demo-app/src/components/MyCar.js
@@ -0,0 +1,5 @@
+function MyCar(){
+  return <h3> This is my Car </h3>
+}
+
+export default MyCar
