1cde10f8541a5c0f9340b2954d2a6649376f0ea7
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-10-01 22:43:17 +0530

    State and useState hook

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,20 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Car from './components/Car'
-import MyCar from './components/MyCar'
-import {GreetUser} from './components/GreetUser'
-import ActorRole from './components/ActorRole'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellow World</h1>
-      <Car />
-      <MyCar />
-      <GreetUser username="Shubham" />
-      <GreetUser username="Abhishek" />
-      <ActorRole name='Clark' role='Spiderman'> This is child text </ActorRole>
-      <ActorRole name='Henry' role='Batman'> <button>Action</button> </ActorRole>
+      <Message />
     </div>
   );
 }
