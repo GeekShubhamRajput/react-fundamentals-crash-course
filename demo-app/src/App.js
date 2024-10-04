953e233d469a25876eebb165b62cb4a125507052
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-10-04 16:42:35 +0530

    useTransiontion hook

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { CreatePost } from './components/CreatePost'
+import NAMES from './components/data.json'
+import {useState, useTransition} from 'react'
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [inputValue, setInputValue] = useState('')
+  const [isPending, startTransition] = useTransition()
+
+  const changeHandler = (event) => {
+    setInputValue(event.target.value)
+    startTransition(() => setQuery(event.target.value))
+  }
+
+  const filteredNames = NAMES.filter(item =>{
+    return item.first_name.includes(query) || item.last_name.includes(query)
+  }) 
+
   return (
     <div className="App">
-      <CreatePost />
+      <input type='text' value={inputValue} onChange={changeHandler} className='mt-5' />
+      { isPending && <p>Updateing list...</p> }
+
+      {
+        filteredNames.map((item) => (
+          <p key={item.id}> {item.first_name + ' ' + item.last_name} </p>  
+        ))
+      }
     </div>
   );
 }
